3a0644e4ed0563e6c52c6b55b3bda3e986359e1a
Pawel Dzierzbicki <ativek23@gmail.com>
2022-03-16 19:32:37 +0100

    added homework5

--- script/script.js
+++ script/script.js
@@ -1,38 +1,48 @@
-let sum = document.querySelector(".js-sum");
-let zloty = document.querySelector(".js-zloty");
-let formElement = document.querySelector(".js-form");
-let result2 = document.querySelector(".result2");
-let result1 = document.querySelector(".result1");
-let currency = document.querySelector(".currency");
-
-let formEuro = document.querySelector(".js-euro");
-let formDolar = document.querySelector(".js-dolar");
-let formFrank = document.querySelector(".js-frank");
-
-formEuro = 4.90;
-formDolar = 4.48;
-formFrank = 4.88;
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
+{
     let result;
-    let sumCurrency = sum.value;
 
-    if (document.querySelector(".js-euro").checked == true) {
-        result = sumCurrency * formEuro;
-        result1.innerText = " Euro ";
-    }
+    const calculateResult = () => {
+        const currency = document.querySelector(".currency");
+        const result1 = document.querySelector(".result1");
 
-    else if (document.querySelector(".js-dolar").checked == true) {
-        result = sumCurrency * formDolar;
-        result1.innerText = " Dolar ";
-    }
-    else if (document.querySelector(".js-frank").checked == true) {
-        result = sumCurrency * formFrank;
-        result1.innerText = " Frank ";
-    }
+        const sum = document.querySelector(".js-sum");
+        const sumCurrency = sum.value;
 
-    currency.innerText = sumCurrency;
-    result2.innerText = result.toFixed(2);
-});
+        const formEuro = 4.90;
+        const formDolar = 4.48;
+        const formFrank = 4.88;
+
+        if (document.querySelector(".js-euro").checked == true) {
+            result = sumCurrency * formEuro;
+            result1.innerText = " Euro ";
+        }
+
+        else if (document.querySelector(".js-dolar").checked == true) {
+            result = sumCurrency * formDolar;
+            result1.innerText = " Dolar ";
+        }
+        else if (document.querySelector(".js-frank").checked == true) {
+            result = sumCurrency * formFrank;
+            result1.innerText = " Frank ";
+        }
+
+        currency.innerText = sumCurrency;
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const result2 = document.querySelector(".result2");
+
+        calculateResult();
+        result2.innerText = result.toFixed(2);
+    };
+    
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
